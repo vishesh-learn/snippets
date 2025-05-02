c2dac18810e371a50697a81192a2524343b12ced
Vishesh <vishesh.bluehance360@gmail.com>
2025-05-02 10:30:30 +0530

    Enhance populateWithWixData function to include filtering and sorting options

--- wix/populateOptions.js
+++ wix/populateOptions.js
@@ -1,10 +1,28 @@
-async function populateWithWixData(collectionName, dropdown, optionLabelFieldId, optionValueFieldId = "_id", additionalOptionsAtStart = [], additionalOptionsAtLast = []) {
+async function populateWithWixData(
+    collectionName,
+    dropdown,
+    optionLabelFieldId,
+    optionValueFieldId,
+    additionalOptionsAtStart = [],
+    additionalOptionsAtLast = [],
+    fieldToCheckIfTrue,
+    sortBy
+) {
     try {
         dropdown.disable();
 
-        let { items } = await wixData.query(collectionName)
-            .limit(1000)
-            .find();
+        let query = wixData.query(collectionName)
+            .limit(1000);
+
+        if (fieldToCheckIfTrue) {
+            query = query.isNotEmpty(fieldToCheckIfTrue);
+        }
+
+        if (typeof sortBy == "string") {
+            query = query.ascending(sortBy);
+        }
+
+        let { items } = await query.find();
 
         let options = items.map(item => ({
             label: optionLabelFieldId ? item[optionLabelFieldId] : item["title"] || item["_id"],
