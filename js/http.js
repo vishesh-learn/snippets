667b636426dc466ec301b784cd45b7d645dd2d35
Vishesh <vishesh.bluehance360@gmail.com>
2025-05-14 15:49:29 +0530

    add fetch code snippet

---
+++ js/http.js
@@ -0,0 +1,22 @@
+let options = {
+    method: 'POST',
+    headers: {
+        'Authorization': 'Basic ' + btoa(`${USERNAME}:${PASSWORD}`),
+        'Content-Type': 'application/json',
+        'body': JSON.stringify(body)
+    }
+}
+
+fetch(API_URL, options)
+    .then(res => {
+        if (res.headers.get('content-type').includes('application/json')) {
+            return res.json();
+        } else {
+            return res.text();
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+        
+        return false;
+    });
